8b7b3d09fc2aeb34e45e85f85be1bae58e3c615b
Shyanukant Rathi <72182251+shyanukant@users.noreply.github.com>
2025-07-31 17:46:53 +0000

    fix image

--- src/components/landing/LoadingScreen.tsx
+++ src/components/landing/LoadingScreen.tsx
@@ -3,7 +3,7 @@ export const LoadingScreen = () => {
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-background to-muted/30">
       <div className=" w-32 h-32 animate-spin-x [transform-style:preserve-3d]">
         <img
-          src="./images/logo.png"
+          src="images/logo.png"
           alt="Loading Logo"
           className="drop-shadow-lg w-32 h-32"
         />
